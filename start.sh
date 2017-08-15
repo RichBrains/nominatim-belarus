00891ef83e1c4229f980da9e5bd487932b0707c1
Daniel Drozdovich <d.drozdovich@richbrains.net>
2017-08-15 18:17:28 +0300

    Auto update

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /app/src;sudo -u nominatim ./utils/update.php --import-osmosis-all --no-npi &
 service postgresql start
+cd /app/src;sudo -u nominatim ./utils/update.php --import-osmosis-all --no-npi &
 /usr/sbin/apache2ctl -D FOREGROUND
