9a40ce9ccc6d068d0a01525df31356b773739e11
Daniel Drozdovich <d.drozdovich@Daniels-MacBook-Pro.local>
2017-11-21 19:41:37 +0300

    Revert refresh interval

--- local.php
+++ local.php
@@ -6,6 +6,6 @@
  @define('CONST_Website_BaseURL', '/');
  @define('CONST_Replication_Url', 'http://download.geofabrik.de/europe/belarus-updates');
  @define('CONST_Replication_MaxInterval', '86400');     // Process each update separately, osmosis cannot merge multiple updates
- @define('CONST_Replication_Update_Interval', '3600');  // How often upstream publishes diffs
+ @define('CONST_Replication_Update_Interval', '86400');  // How often upstream publishes diffs
  @define('CONST_Replication_Recheck_Interval', '900');   // How long to sleep if no update found yet
 ?>
