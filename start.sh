3ca5600df514244b4497631ef1cddbce28de6048
Daniel <snoopckuu@gmail.com>
2017-12-13 14:33:16 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 service postgresql start
-cd /app/Nominatim-3.0.0/build;sudo -u nominatim ./utils/update.php --import-osmosis-all &
+cd /app/Nominatim/build;sudo -u nominatim ./utils/update.php --import-osmosis-all &
 /usr/sbin/apache2ctl -D FOREGROUND
